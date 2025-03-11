7ce976c6ef4e4f8aff6a2f9d117a0288ad720666
h zy <2658628026@qq.com>
2025-03-11 22:12:21 +0800

    training scripts

---
+++ scripts/run_train.sh
@@ -0,0 +1,16 @@
+mkdir data/
+mkdir data/selfplay/
+mkdir data/models/
+export LD_LIBRARY_PATH=LD_LIBRARY_PATH:"/root/lib/"
+while true
+do
+    chmod +x ./engine/katago
+    CUDA_VISIBLE_DEVICES="0" ./engine/katago selfplay -models-dir  data/models -config selfplay.cfg -output-dir data/selfplay  -max-games-total 40000
+    cd train
+    bash shuffle.sh ../data ./ktmp 16 512
+    CUDA_VISIBLE_DEVICES="0" bash train.sh ../data b10c256n b10c256nbt-mish 512 main
+    
+    CUDA_VISIBLE_DEVICES="0" bash export.sh connectfour ../data 0
+    python view_loss.py
+    cd ..
+done
