56abc40f2931e785ed7bbc645a24a25da75363df
h zy <2658628026@qq.com>
2025-05-04 19:42:19 +0800

    default not define FORGOMOCUP in config

--- cpp/core/config.h
+++ cpp/core/config.h
@@ -1,7 +1,7 @@
 // options
 //#define NOVCF                     //if defined, then not use vcf
 //#define CHANGE_FORBIDDEN_POLICY   //set policy target of forbidden points to nonzero
-#define FORGOMOCUP                //CPU only and single thread
+//#define FORGOMOCUP                //CPU only and single thread
 
 #ifndef COMPILE_MAX_BOARD_LEN
 #define COMPILE_MAX_BOARD_LEN 15 // Board::MAX_LEN, Normal gomoku/renju games are on 15x15 board
