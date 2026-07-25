6e9508b735d2d2eceddafb95f6423eacf5d5e0de
hzy <2658628026@qq.com>
2026-07-25 21:43:20 +0800

    fix a bug of selfplay model detection

--- cpp/dataio/loadmodel.cpp
+++ cpp/dataio/loadmodel.cpp
@@ -22,31 +22,31 @@ static const vector<string> ACCEPTABLE_MODEL_SUFFIXES {
   ".bin",
   ".onnx",
   "model.txt.gz",
-  "model.txt"
+  "model.txt",
 };
 static const vector<string> GENERIC_MODEL_NAMES {
   "model.bin.gz",
   "model.bin",
   "model.txt.gz",
-  "model.txt"
+  "model.txt",
   "Model.bin.gz",
   "Model.bin",
   "Model.txt.gz",
-  "Model.txt"
+  "Model.txt",
   "MODEL.bin.gz",
   "MODEL.bin",
   "MODEL.txt.gz",
-  "MODEL.txt"
+  "MODEL.txt",
   "model.onnx",
-  "MODEL.onnx"
+  "MODEL.onnx",
   "model.ckpt",
-  "Model.ckpt"
+  "Model.ckpt",
   "MODEL.ckpt",
   "model.checkpoint",
-  "Model.checkpoint"
+  "Model.checkpoint",
   "MODEL.checkpoint",
   "model",
-  "Model"
+  "Model",
   "MODEL",
 };
 
